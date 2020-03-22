70779ce54efe682d407eacfe860ee23e72021019
Yang Mingshan <y.mingshan3@gmail.com>
2020-03-22 19:02:17 +0800

    chore: add `use strict` in config js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
   '**/*.js': (filenames) => [
     `cross-env NODE_ENV=production eslint --fix ${filenames.join(' ')}`,
--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
   semi: false,
   singleQuote: true,
