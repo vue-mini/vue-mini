163ae5c8d8ec1c05bd98e5631ad20e0aee730609
Yang Mingshan <y.mingshan3@gmail.com>
2020-05-31 20:13:30 +0800

    docs: home page

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'zh-cmn-Hans',
+    },
+  },
+  title: 'Vue Mini',
+  description: '像写 Vue 3 一样写小程序',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+}
