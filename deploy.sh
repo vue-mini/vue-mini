647590844fdcfd9ceebb0aec3c366d3a9976e34e
Yang Mingshan <y.mingshan3@gmail.com>
2020-06-20 19:26:15 +0800

    docs: deploy fix

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ yarn docs:build
 
 cd docs/.vuepress/dist
 
+git config user.email 'y.mingshan3@gmail.com'
+git config user.name 'Yang Mingshan'
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://$ACCESS_TOKEN@github.com/vue-mini/vue-mini.github.io.git master
+git push -f https://yangmingshan:$ACCESS_TOKEN@github.com/vue-mini/vue-mini.github.io.git master
 
 cd -
