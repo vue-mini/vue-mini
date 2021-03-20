50f56ff647bca93ed738faa5bd76badd50a30fa6
Yang Mingshan <y.mingshan3@gmail.com>
2021-03-20 18:06:30 +0800

    docs: add cloudflare web analytics for gitee mirror

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -11,10 +11,11 @@ export default {
       analyticsScript.defer = true
       analyticsScript.src =
         'https://static.cloudflareinsights.com/beacon.min.js'
-      analyticsScript.setAttribute(
-        'data-cf-beacon',
-        '{"token": "860f4aaff387460faaf6c4a62ffa8a63"}'
-      )
+      analyticsScript.dataset.cfBeacon = `{"token": "${
+        location.hostname === 'vue-mini.github.io'
+          ? '860f4aaff387460faaf6c4a62ffa8a63'
+          : '3ee00252b4e1416e9f2e9379d35bf113'
+      }"}`
       document.body.append(analyticsScript)
     }
   },
