c64041677e3ad52ef87e06282e7a855fef7ea895
Yang Mingshan <y.mingshan3@gmail.com>
2024-06-15 16:31:06 +0800

    fix(watch): deep watching symbol properties
    https://github.com/vuejs/core/pull/10969

--- packages/core/__tests__/watch.spec.ts
+++ packages/core/__tests__/watch.spec.ts
@@ -486,6 +486,57 @@ describe('watch', () => {
     expect(dummy).toEqual([1, 2])
   })
 
+  it('deep with symbols', async () => {
+    // eslint-disable-next-line symbol-description
+    const symbol1 = Symbol()
+    // eslint-disable-next-line symbol-description
+    const symbol2 = Symbol()
+    // eslint-disable-next-line symbol-description
+    const symbol3 = Symbol()
+    // eslint-disable-next-line symbol-description
+    const symbol4 = Symbol()
+
+    const raw: any = {
+      [symbol1]: {
+        [symbol2]: 1,
+      },
+    }
+
+    Object.defineProperty(raw, symbol3, {
+      writable: true,
+      enumerable: false,
+      value: 1,
+    })
+
+    const state = reactive(raw)
+    const spy = vi.fn()
+
+    watch(() => state, spy, { deep: true })
+
+    await nextTick()
+    expect(spy).toHaveBeenCalledTimes(0)
+
+    state[symbol1][symbol2] = 2
+    await nextTick()
+    expect(spy).toHaveBeenCalledTimes(1)
+
+    // Non-enumerable properties don't trigger deep watchers
+    state[symbol3] = 3
+    await nextTick()
+    expect(spy).toHaveBeenCalledTimes(1)
+
+    // Adding a new symbol property
+    state[symbol4] = 1
+    await nextTick()
+    expect(spy).toHaveBeenCalledTimes(2)
+
+    // Removing a symbol property
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    delete state[symbol4]
+    await nextTick()
+    expect(spy).toHaveBeenCalledTimes(3)
+  })
+
   it('immediate', async () => {
     const count = ref(0)
     const cb = vi.fn()
